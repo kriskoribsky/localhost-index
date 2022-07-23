d2d05f6ae5c403e2f553b623d1e116d0f03ba0e1
kriskoribsky <kristian.koribsky@gmail.com>
2022-07-23 13:28:39 +0200

    Add new quotes, finalize quote sections

--- index.php
+++ index.php
@@ -20,7 +20,42 @@
         'Imagination is everything. It is the preview of life\'s coming attractions.' => 'Albert Einstein',
         'Creativity is seeing what others see and thinking what no one else ever thought.' => 'Albert Einstein',
         'The true sign of intelligence is not knowledge, but imagination.' => 'Albert Einstein',
-        
+        'We cannot solve our problems with the same thinking we used when we created them.' => 'Albert Einstein',
+        'I have no special talent. I am only passionately curious.' => 'Albert Einstein',
+        'The only reason for time is so that everything doesn\'t happen at once.' => 'Albert Einstein',
+        'Only a life lived for others is a life worthwhile.' => 'Albert Einstein',
+        'It should be possible to explain the laws of physics to a barmaid.' => 'Albert Einstein',
+        'Let us not forget that human knowledge and skills alone cannot lead humanity to a happy and dignified life.' => 'Albert Einstein',
+
+        // Marcus Aurelius
+        'You have power over your mind - not outside events. Realize this, and you will find strength.' => 'Marcus Aurelius, Meditations',
+        'Dwell on the beauty of life. Watch the stars, and see yourself running with them.' => 'Marcus Aurelius, Meditations',
+        'The happiness of your life depends upon the quality of your thoughts.' => 'Marcus Aurelius, Meditations',
+        'Everything we hear is an opinion, not a fact. Everything we see is a perspective, not the truth.' => 'Marcus Aurelius, Meditations',
+        'Waste no more time arguing about what a good man should be. Be one.' => 'Marcus Aurelius, Meditations',
+        'If you are distressed by anything external, the pain is not due to the thing itself, but to your estimate of it; and this you have the power to revoke at any moment.' => 'Marcus Aurelius, Meditations',
+        'When you arise in the morning think of what a privilege it is to be alive, to think, to enjoy, to love ...' => 'Marcus Aurelius, Meditations',
+        'The best revenge is to be unlike him who performed the injury.' => 'Marcus Aurelius, Meditations',
+        'Accept the things to which fate binds you, and love the people with whom fate brings you together,but do so with all your heart.' => 'Marcus Aurelius, Meditations',
+        'The soul becomes dyed with the colour of its thoughts.' => 'Marcus Aurelius, Meditations',
+        'It is not death that a man should fear, but he should fear never beginning to live.' => 'Marcus Aurelius, Meditations',
+        'Never let the future disturb you. You will meet it, if you have to, with the same weapons of reason which today arm you against the present.' => 'Marcus Aurelius, Meditations',
+        'How much more grievous are the consequences of anger than the causes of it.' => 'Marcus Aurelius, Meditations',
+
+        // Steve Jobs
+        'Creativity is just connecting things. When you ask creative people how they did something, they feel a little guilty because they didn\'t really do it, they just saw something. It seemed obvious to them after a while.' => 'Steve Jobs',
+        'If you really look closely, most overnight successes took a long time.' => 'Steve Jobs',
+        'Innovation distinguishes between a leader and a follower.' => 'Steve Jobs',
+        'Your time is limited, so don\'t waste it living someone else\'s life.' => 'Steve Jobs',
+        'Don\'t let the noise of others\' opinions drown out your own inner voice.' => 'Steve Jobs',
+        'You can\'t connect the dots looking forward; you can only connect them looking backwards. So you have to trust that the dots will somehow connect in your future.' => 'Steve Jobs',
+        'Be a yardstick of quality. Some people aren\'t used to an environment where excellence is expected.' => 'Steve Jobs',
+        'Stay hungry. Stay foolish.' => 'Steve Jobs',
+        'I\'m convinced that about half of what separates the successful entrepreneurs from the nonsuccessful ones is pure perseverance.' => 'Steve Jobs',
+        'You can\'t just ask customers what they want and then try to give that to them. By the time you get it built, they\'ll want something new.' => 'Steve Jobs',
+        'People think focus means saying yes to the thing you\'ve got to focus on. It means saying no to the hundred other good ideas that there are. You have to pick carefully.' => 'Steve Jobs',
+        'We\'re here to put a dent in the universe. Otherwise why else even be here?' => 'Steve Jobs',
+
         // Busines
         'Success is not final; failure is not fatal: it is the courage to continue that counts.' => 'Winston Churchill',
         'Play by the rules, but be ferocious.' => 'Phil Knight',
@@ -31,7 +66,6 @@
         'I owe my success to having listened respectfully to the very best advice, and then going away and doing the exact opposite.' => 'G.K. Chesterton',
         'I don\'t know the word \'quit.\' Either I never did, or I have abolished it.' => 'Susan Butcher',
         'Far and away the best prize that life offers is the chance to work hard at work worth doing.' => 'Theodore Roosevelt',
-        'If you really look closely, most overnight successes took a long time.' => 'Steve Jobs',
         'Almost everything worthwhile carries with it some sort of risk, whether it\'s starting a new business, whether it\'s leaving home, whether it\'s getting married, or whether it\'s flying into space.' => 'Chris Hadfield',
         'Even if you are on the right track, you\'ll get run over if you just sit there.' => 'Will Rodgers',
         'The real test is not whether you avoid this failure, because you won\'t. It\'s whether you let it harden or shame you into inaction, or whether you learn from it; whether you choose to persevere.' => 'Barack Obama',
@@ -67,7 +101,6 @@
         'Make visible what, without you, might perhaps never have been seen.' => 'Oprah Winfrey',
         'Simplicity is the ultimate sophistication.' => 'Leonardo Da Vinci',
         'Draw the art you want to see, start the business you want to run, play the music you want to hear, write the books you want to read, build the products you want to use - do the work you want to see done.' => 'Austin Kleon',
-        'Creativity is just connecting things. When you ask creative people how they did something, they feel a little guilty because they didn\'t really do it, they just saw something. It seemed obvious to them after a while.' => 'Steve Jobs',
         'Everything you can imagine is real.' => 'Pablo Picasso',
         'Imagination is the beginning of creation. You imagine what you desire, you will what you imagine, and at last, you create what you will.' => 'George Bernard Shaw',
         'Creativity is… seeing something that doesn\'t exist already. You need to find out how you can bring it into being and that way be a playmate with God.' => 'Michele Shea',
@@ -95,6 +128,23 @@
         'Before software can be reusable it first has to be usable.' => 'Ralph Johnson',
         'Make it work, make it right, make it fast.' => 'Kent Beck',
 
+        // Hacking
+        'Amateurs hack systems, professionals hack people.' => 'Bruce Schneier',
+        'If you spend more on coffee than on IT security, you will be hacked. What\'s more, you deserve to be hacked.' => 'Richard Clarke',
+        'Time is what determines security. With enough time nothing is unhackable.' => 'Aniekee Tochukwu Ezekiel',
+        'Yes, I am a criminal. My crime is that of curiosity. My crime is that of outsmarting you, something that you will never forgive me for. I am a hacker, and this is my manifesto. You may stop this individual, but you can\'t stop us all… after all, we\'re all alike.' => 'The Mentor (Loyd Blankenship)',
+        'No technology that\'s connected to the Internet is unhackable.' => 'Abhijit Naskar',
+        'As a matter of fact, yeah, they were foolproof. The problem is that you don\'t have to protect yourself against fools. You have to protect yourself against people like me.' => 'Jeffery Deaver',
+        'Never underestimate the determination of a kid who is time-rich and cash-poor.' => 'Cory Doctorow',
+        'It is a fairly open secret that almost all system can be hacked, somehow. It is a less spoken of secret that such hacking has actually gone quite mainstream.' => 'Dan Kaminsky',
+        'Most hackers are young because young people tend to be adaptable. As long as you remain adaptable, you can always be a good hacker.' => 'Emmanuel Goldstein',
+        'I was addicted to hacking, more for the intellectual challenge, the curiosity, the seduction of adventure; not for stealing, or causing damage or writing computer viruses.' => 'Kevin Mitnick',
+        'One single vulnerability all an attacker needs.' => 'Window Snyder',
+        'For every lock, there is someone out there trying to pick it or break in.' => 'David Bernstein',
+        'There are more hackers breeding every day, and more brilliant minds are turning into hackers. Security has advanced, but so have hackers.' => 'Michael Demon Calce',
+        'While many hackers have the knowledge, skills, and tools to attack computer systems, they generally lack the motivation to cause violence or severe economic or social harm.' => 'Dorothy E. Denning',
+        'The hacker mindset doesn\'t actually see what happens on the other side, to the victim.' => 'Kevin Mitnick',
+
         // Others
         'There is nothing impossible to him who will try.' => 'Alexander the Great',
         'When you have a dream, you\'ve got to grab it and never let go.' => 'Carol Burnett',
@@ -102,7 +152,9 @@
         'Success is not final, failure is not fatal: it is the courage to continue that counts.' => 'Winston Churchill',
         'You are never too old to set another goal or to dream a new dream.' => 'Malala Yousafzai',
         'Where your fear is, there is your task.' => 'Carl Jung',
-        'Everything you ever wanted is on the other side of fear.' => 'George Adair'
+        'Everything you ever wanted is on the other side of fear.' => 'George Adair',
+        'You must understand that there is more than one path to the top of the mountain.' => 'Miyamoto Musashi, A Book of Five Rings',
+        'A ship in harbor is safe, but that is not what ships are built for.' => 'John A. Shedd'
     ]);
     
     class Helper {
@@ -456,6 +508,10 @@
             content: '"';
         }
 
+        .author::before {
+            content: '― ';
+        }
+
     </style>
 
 </head>
@@ -483,7 +539,7 @@
             <table class="container primary-text-clr" width="100%">
 
                 <thead>
-                    
+
                     <tr class="sort-rows">
                         <!-- GET requests using href & sort order logic -->
                         <th abbr="File names" title="Sort alphabetically">
@@ -554,7 +610,7 @@
             <blockquote class="container">
 
                     <p class="quote"><?php echo $random_quote; ?></p>
-                    <p class="text-right"><strong><?php echo QUOTES[$random_quote]; ?></strong></p>
+                    <p class="author text-right"><strong><?php echo QUOTES[$random_quote]; ?></strong></p>
 
             </blockquote>
 
